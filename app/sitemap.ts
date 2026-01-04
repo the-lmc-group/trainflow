1f4bc934e9b931a7d3bd43e55c972fbf1256f002
lotus64 <lotus64santon@gmail.com>
2026-01-04 17:29:26 +0100

    Fix: Update base URL in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://trainflow.io";
+  const baseUrl = "https://trainflow.lmcgroup.xyz";
 
   return [
     {
